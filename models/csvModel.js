89122f87ed0ebd5be654368ed21b9d4edac7880d
kin6sman <domin12313xyz@gmail.com>
2023-06-16 13:06:34 +0530

    schema created

--- models/csvModel.js
+++ models/csvModel.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose');
 
 const csvSchema = new mongoose.Schema({
-  name: String,
-  age: Number,
-  email: String
+  filename: {
+    type: String,
+    required: true,
+  },
+  header_row: {
+    type: [Object],
+  },
+  data_rows: {
+    type: [Object],
+  },
+}, {
+  timestamps: true,
 });
 
 module.exports = mongoose.model('CSV', csvSchema);
